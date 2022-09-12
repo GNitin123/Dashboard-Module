b57b5636a258724ada7d6af34ead7ba36e60a404
G Nitin <nitin.naik@rayabharitech.com>
2022-09-12 21:28:43 +0530

    added initial Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import '../src/assets/scss/styles.scss'
 import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
 
 const App = () => {
-  return <div>APP</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
