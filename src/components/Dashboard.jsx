fb7f8a9d961f95d188449eb4a3a49a34e1d0acb6
G Nitin <nitin.naik@rayabharitech.com>
2022-09-13 17:08:00 +0530

    handled page header for dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import defaultLayout from '../layout/defaultLayout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlane, faPlus, faStar } from '@fortawesome/free-solid-svg-icons'
+import { Button } from 'react-bootstrap'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div className="component-dashboard">
+      <div className="component-dashboard__page-header">
+        <div className="component-dashboard__page-header__title-wrapper">
+          <div className="component-dashboard__page-header__icon">
+            <FontAwesomeIcon color={'#1e73be'} icon={faPlane} size="2x" />
+          </div>
+          <div className="component-dashboard__page-header__title">
+            <h4>Analytics Dashboard</h4>
+            <p>
+              Dashboard is a online report where your website data from e-commerce to web analytics
+              is displayed
+            </p>
+          </div>
+        </div>
+        <div>
+          <Button variant="light" className="component-dashboard__page-header__star-btn">
+            <FontAwesomeIcon icon={faStar} />
+          </Button>
+          <Button variant="success">
+            <FontAwesomeIcon icon={faPlus} /> Create New
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default defaultLayout(Dashboard)
