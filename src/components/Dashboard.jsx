e271f76726f1f55868980bdcefceaec4650b7e13
G Nitin <nitin.naik@rayabharitech.com>
2022-09-14 10:05:58 +0530

    Completed Task Module

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import { useContext, useState } from 'react'
 import defaultLayout from '../layout/defaultLayout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlane, faPlus, faStar, faDatabase, faPrint } from '@fortawesome/free-solid-svg-icons'
-import { Button, Form, InputGroup } from 'react-bootstrap'
+import { Button, Form, InputGroup, Badge, Image } from 'react-bootstrap'
 import Card from './Card'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import DashboardContext from '../context/DashboardContext'
 
 const Dashboard = () => {
+  const { taskList, setTaskList } = useContext(DashboardContext)
+  const [isTaskListOrderChanged, setIsTaskListOrderChanged] = useState(false)
+
+  const onDragEnd = result => {
+    const { source, destination } = result
+    if (!destination) return
+    if (source.index == destination.index) {
+      return
+    } else {
+      setIsTaskListOrderChanged(true)
+    }
+    const items = Array.from(taskList)
+    const [reorderItem] = items.splice(source.index, 1)
+    items.splice(destination.index, 0, reorderItem)
+    items && setTaskList(items)
+  }
+
+  const handleSave = () => taskList.map((task, index) => console.log(`Task ${index + 1}:`, task))
+
   const TaskListFooter = (
     <div className="component-dashboard__main-section__task-btn-wrapper">
-      <Button variant="light">Cancel</Button>
-      <Button variant="primary">save</Button>
+      <Button variant="light" onClick={() => setIsTaskListOrderChanged(false)}>
+        Cancel
+      </Button>
+      <Button variant="primary" onClick={handleSave}>
+        save
+      </Button>
     </div>
   )
 
@@ -23,6 +48,36 @@ const Dashboard = () => {
       <Button variant="success">Send</Button>
     </InputGroup>
   )
+
+  const Task = taskList.map((task, index) => (
+    <Draggable draggableId={index + task} key={index} index={index}>
+      {provided => (
+        <div
+          className="component-dashboard__main-section__tasks"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <Form.Check />
+          <div className="component-dashboard__main-section__task-desc">
+            {task.imageURL && (
+              <Image src={task?.imageURL} className="component-dashboard__main-section__img" />
+            )}
+            <div>
+              <h5>
+                {task?.title}{' '}
+                <Badge bg={task?.type} className="ms-1">
+                  {task?.status}
+                </Badge>
+              </h5>
+              <p>{task?.description}</p>
+            </div>
+          </div>
+          {provided.placeholder}
+        </div>
+      )}
+    </Draggable>
+  ))
 
   return (
     <div className="component-dashboard">
@@ -49,7 +104,27 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="component-dashboard__main-section">
-        <Card title="Task Lists" icon={faDatabase} color="#1e73be" footer={TaskListFooter} />
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Card
+            title="Task Lists"
+            icon={faDatabase}
+            color="#1e73be"
+            footer={isTaskListOrderChanged && TaskListFooter}
+          >
+            <Droppable droppableId="taskList">
+              {provided => (
+                <div
+                  className="component-dashboard__main-section__droppable"
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {Task}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </Card>
+        </DragDropContext>
         <Card title="Chat Box" icon={faPrint} color="#e41638" footer={chatBoxFooter} />
       </div>
     </div>
